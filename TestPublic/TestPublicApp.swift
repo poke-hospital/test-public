aa03f6260684827ef1254e72e09d20a1a69a84cc
Yiqing Zhao <cissyzhao99@gmail.com>
2026-04-03 13:30:07 +0800

    init

---
+++ TestPublic/TestPublicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPublicApp.swift
+//  TestPublic
+//
+//  Created by Yiqing Zhao on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TestPublicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestPublicTests/TestPublicTests.swift
@@ -0,0 +1,19 @@
+//
+//  TestPublicTests.swift
+//  TestPublicTests
+//
+//  Created by Yiqing Zhao on 4/3/26.
+//
+
+import Testing
+@testable import TestPublic
+
+struct TestPublicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
